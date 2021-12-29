c1577f3183bab6e73b6955ab816409972fc2cd74
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2021-12-30 00:17:47 +0900

    [Add] room entity setting

---
+++ app/src/main/java/com/mobinity/mvvm_dagger_practice/room/BlogDatabase.kt
@@ -0,0 +1,17 @@
+package com.mobinity.mvvm_dagger_practice.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [BlogCacheEntity::class], version = 1)
+abstract class BlogDatabase: RoomDatabase() {
+
+    abstract fun blogDao(): BlogDao
+
+    companion object {
+
+        val DATABASE_NAME: String = "blog_db"
+    }
+
+}
